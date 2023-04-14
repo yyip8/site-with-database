7d6e6232d98160eefc6776cd3d2512eba3dfaf81
yyip8 <yyip8@my.centennialcollege.ca>
2023-04-14 17:14:55 -0400

    mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 module.exports =
  {
    //Include your URI Here to connect to the database
-   // "URI":"mongodb+srv://anubhav8080:qG6UvifzmkLtvv8Y@cluster0.mpbedcl.mongodb.net/?retryWrites=true&w=majority",
-   "URI": "mongodb+srv://yyip8:GkGa6b2zGDuy9dEG@cluster0.kidsifp.mongodb.net/GoodRead?retryWrites=true&w=majority",
+   "URI":"mongodb+srv://anubhav8080:qG6UvifzmkLtvv8Y@cluster0.mpbedcl.mongodb.net/?retryWrites=true&w=majority",
+   // "URI": "mongodb+srv://yyip8:GkGa6b2zGDuy9dEG@cluster0.kidsifp.mongodb.net/GoodRead?retryWrites=true&w=majority",
    "Secret": 'SomeSecret'
 
 }
